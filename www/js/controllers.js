d93664c30517dc6e7cce9dfcb7e2add97c82a60b
bobilincho <bobilincho@gmail.com>
2017-01-23 15:41:42 +0200

    three js added

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,8 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope,THREE) {
+  console.log(THREE)
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
